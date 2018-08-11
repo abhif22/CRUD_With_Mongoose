8d3729d652da3e4bd3ff67d83f2da3a639b49922
abhif22 <abhishek.srivastav@live.in>
2018-08-11 15:53:49 +0530

    Class Based Removal

--- test/user.test.js
+++ test/user.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const mongoose = require('mongoose');
 const User = require('../src/models/user');
 var newUser;
-describe('Testing User Model', ()=>{
+describe('Testing User Model For Creating and Searching ', ()=>{
     before((done)=>{
         mongoose.connection.collections.users.drop(()=>{
             done();
@@ -23,4 +23,35 @@ describe('Testing User Model', ()=>{
                 done();
             })
     })
+});
+
+describe('Testing User Model for Deletion', ()=>{
+    beforeEach((done)=>{
+        newUser = new User({name: 'Abhishek Srivastava'});
+        newUser.save()
+            .then(()=>done());
+    });
+    it('Using Instance Based Method to Remove', (done)=>{
+        newUser.remove()
+            .then(()=> {
+                User.findOne({name: 'Abhishek Srivastava'})
+            })
+            .then((result)=>{
+                assert(result==null);
+                done();
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+    });
+    it('Using Class based remove', (done)=>{
+        User.remove({name: 'Abhishek Srivastava'})
+            .then(()=>{
+                User.findOne({name: 'Abhishek Srivastava'})
+            })
+            .then((result)=>{
+                assert(result==null);
+                done();
+            });
+    });
 });
