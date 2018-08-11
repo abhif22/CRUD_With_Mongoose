a9488ba1100b332a7e778182dae0e585daeb6977
abhif22 <abhishek.srivastav@live.in>
2018-08-11 18:35:42 +0530

    Various Ways t Update in mongoose

--- test/user.test.js
+++ test/user.test.js
@@ -75,3 +75,69 @@ describe('Testing User Model for Deletion', ()=>{
             });
     });
 });
+
+describe('Testing User Model Updation', ()=>{
+    beforeEach('First Inserting Records for updation', (done)=>{
+        newUser = new User({name: 'Abhishek Srivastava'});
+        newUser.save()
+            .then(()=> done());
+    }); 
+    it('Using Instance Based Method \'Set and Update\' ', (done)=>{
+        newUser.set('name', 'Surbhi Srivastava');
+        newUser.save()
+            .then(()=>{
+                return User.findOne({name: 'Surbhi Srivastava'})
+            })
+            .then((result)=>{
+                // console.log(`Result: ${result}`);
+                assert(result!=null);
+                assert(result.name.toString()==='Surbhi Srivastava');
+                done();
+            })
+            .catch((err)=> console.log(err));
+    });
+    it('Using Instance based Update()', (done)=>{
+        newUser.update({name: 'Mansi Srivastava'}, {new: true})
+            .then((data)=>{
+                return User.findOne({name: 'Mansi Srivastava'})
+            })
+            .then((result)=>{
+                assert(result!=null);
+                assert(result.name.toString()==='Mansi Srivastava');
+                done();
+            });
+    });
+    it('Using Class Based update()', (done)=>{
+        User.update({name: 'Abhishek Srivastava'}, {name: 'Sudha Srivastava'}, {new: true})
+            .then(()=>{
+                return User.findOne({name: 'Sudha Srivastava'})
+            })
+            .then((result)=>{
+                assert(result!=null);
+                assert(result.name.toString()==='Sudha Srivastava');
+                done();
+            });
+    });
+    it('Using Class Based findOneAndUpdate()', (done)=>{
+        User.findOneAndUpdate({name: 'Abhishek Srivastava'}, {name: 'Manoj Srivastava'}, {new: true})
+            .then(()=>{
+                return User.findOne({name: 'Manoj Srivastava'})
+            })
+            .then((result)=>{
+                assert(result!=null);
+                assert(result.name.toString()==='Manoj Srivastava');
+                done();
+            });
+    });
+    it('Using Class Based findByIdAndUpdate()', (done)=>{
+        User.findByIdAndUpdate({_id: newUser.id}, {name: 'Srivastava'}, {new: true})
+            .then(()=>{
+                return User.findOne({_id: newUser.id})
+            })
+            .then((result)=>{
+                assert(result!=null);
+                assert(result.name.toString()==='Srivastava');
+                done();
+            });
+    });
+});
